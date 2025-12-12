ddb2aa7a2b813459cd13a0be8b65ec931d2a1281
tahashieenavaz <tahashieenavaz@gmail.com>
2025-12-12 23:27:25 +0100

    removes extra space

--- simple-cnn.py
+++ simple-cnn.py
@@ -2,7 +2,6 @@ from flax import linen as nn
 from jax.numpy import jnp
 
 class SimpleConvolutionalLayer(nn.Module):
-
     @nn.compact
     def __call__(self, x):
         x = nn.Conv(3, 16, kernel_size=3)(x)
@@ -10,3 +9,4 @@ class SimpleConvolutionalLayer(nn.Module):
         x = nn.Conv(16, 32, kernel_size=3)(x)
         x = nn.relu(x)
         return x
+    
