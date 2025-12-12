6b74411d23f39c791046b339100bfa03ed0bbb5b
tahashieenavaz <tahashieenavaz@gmail.com>
2025-12-12 20:11:25 +0100

    adds appropriate functions

--- linear-regression.py
+++ linear-regression.py
@@ -2,6 +2,16 @@ import jax
 import jax.numpy as jnp
 from jax import grad
 
+def predict(params, x):
+    w,b =params
+    return w * x + b
+
+def loss(params, x, y):
+    preds = predict(params, x)
+    return jnp.mean((preds - y) ** 2)
+
+
+
 key = jax.random.PRNGKey(0)
 x = jax.random.normal(key, (100, 1))
 
@@ -9,7 +19,3 @@ a = 2.0
 b = -3.5
 
 y = a * x[:, 0] + b + jax.random.normal(key, (100,))
-
-def predict(params, x):
-    w,b =params
-    return w * x + b
